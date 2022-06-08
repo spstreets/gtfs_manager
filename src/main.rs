ef7d4618fa6c33ad1618ff2f266c5682f0be1743
Max Unsted <maxunsted@gmail.com>
2022-06-08 12:01:29 +0100

    Only use usize indexes in data model
    Replace GTFS data in AppData with usize indexes of the corresponding
    row in the RawGtfs table so see if this improves loading or closing
    performance of the app.
    Doesn't seem to have any noticeable effect. The problem is probably that
    Druid is creating every single widget for every item in the data.
    Instead, only widgets for the top level items should be created up front
    and all other widgets created lazily on demand.

--- src/main.rs
+++ src/main.rs
@@ -32,12 +32,13 @@ pub struct CliArgs {
 #[derive(Clone, Data, Default, Lens)]
 struct MyStopTime {
     selected: bool,
-    stop_sequence: u16,
+    // stop_sequence: u16,
     // #[data(ignore)]
     // #[lens(ignore)]
     // stop_time: Rc<RawStopTime>,
     // stop_time: RawStopTime,
-    name: String,
+    // name: String,
+    index: usize,
 }
 
 #[derive(Clone, Data, Default, Lens)]
@@ -45,23 +46,26 @@ struct MyTrip {
     selected: bool,
     // #[data(ignore)]
     // trip: RawTrip,
-    name: String,
+    // name: String,
+    index: usize,
     stops: Vector<MyStopTime>,
 }
 
 #[derive(Clone, Data, Default, Lens)]
 struct MyRoute {
     selected: bool,
-    #[data(ignore)]
-    route: Route,
+    index: usize,
+    // #[data(ignore)]
+    // route: Route,
     trips: Vector<MyTrip>,
 }
 
 #[derive(Clone, Data, Default, Lens)]
 struct MyAgency {
     selected: bool,
-    #[data(ignore)]
-    agency: Agency,
+    index: usize,
+    // #[data(ignore)]
+    // agency: Agency,
     routes: Vector<MyRoute>,
 }
 
@@ -70,11 +74,12 @@ struct AppData {
     agencies: Vector<MyAgency>,
 }
 
-fn stop_ui() -> impl Widget<MyStopTime> {
+fn stop_ui(gtfs: Rc<RawGtfs>) -> impl Widget<MyStopTime> {
+    let gtfs2 = Rc::clone(&gtfs);
     Flex::row()
         .with_child(Checkbox::new("").lens(MyStopTime::selected))
-        .with_child(Label::new(|data: &MyStopTime, _env: &_| {
-            format!("{}", data.name)
+        .with_child(Label::new(move |data: &MyStopTime, _env: &_| {
+            format!("{}", gtfs2.stop_times.as_ref().unwrap()[data.index].stop_id)
         }))
         // .with_child(Either::new(
         //     |data: &Trip, _env: &Env| data.selected,
@@ -84,68 +89,71 @@ fn stop_ui() -> impl Widget<MyStopTime> {
         .cross_axis_alignment(CrossAxisAlignment::Start)
         .padding((10., 0., 0., 0.))
 }
-fn trip_ui() -> impl Widget<MyTrip> {
+fn trip_ui(gtfs: Rc<RawGtfs>) -> impl Widget<MyTrip> {
+    let gtfs2 = Rc::clone(&gtfs);
     let title = Flex::row()
         .with_child(Checkbox::new("").lens(MyTrip::selected))
-        .with_child(Label::new(|data: &MyTrip, _env: &_| {
-            format!("{}", data.name)
+        .with_child(Label::new(move |data: &MyTrip, _env: &_| {
+            format!("{}", gtfs2.trips.as_ref().unwrap()[data.index].id)
         }));
 
     Flex::column()
         .with_child(title)
         .with_child(Either::new(
             |data: &MyTrip, _env: &Env| data.selected,
-            List::new(stop_ui).lens(MyTrip::stops),
+            List::new(move || stop_ui(Rc::clone(&gtfs))).lens(MyTrip::stops),
             Flex::row(),
         ))
         .cross_axis_alignment(CrossAxisAlignment::Start)
         .padding((10., 0., 0., 0.))
 }
-fn route_ui() -> impl Widget<MyRoute> {
+fn route_ui(gtfs: Rc<RawGtfs>) -> impl Widget<MyRoute> {
+    let gtfs2 = Rc::clone(&gtfs);
     let title = Flex::row()
         .with_child(Checkbox::new("").lens(MyRoute::selected))
-        .with_child(Label::new(|data: &MyRoute, _env: &_| {
-            format!("{}", data.route.short_name)
+        .with_child(Label::new(move |data: &MyRoute, _env: &_| {
+            format!("{}", gtfs2.routes.as_ref().unwrap()[data.index].short_name)
         }));
 
     Flex::column()
         .with_child(title)
         .with_child(Either::new(
             |data: &MyRoute, _env: &Env| data.selected,
-            List::new(trip_ui).lens(MyRoute::trips),
+            List::new(move || trip_ui(Rc::clone(&gtfs))).lens(MyRoute::trips),
             Flex::row(),
         ))
         .cross_axis_alignment(CrossAxisAlignment::Start)
         .padding((10., 0., 0., 0.))
 }
-fn agency_ui() -> impl Widget<MyAgency> {
+fn agency_ui(gtfs: Rc<RawGtfs>) -> impl Widget<MyAgency> {
+    let gtfs2 = Rc::clone(&gtfs);
     let title = Flex::row()
         .with_child(Checkbox::new("").lens(MyAgency::selected))
-        .with_child(Label::new(|data: &MyAgency, _env: &_| {
-            format!("{}", data.agency.name)
+        .with_child(Label::new(move |data: &MyAgency, _env: &_| {
+            format!("{}", gtfs2.agencies.as_ref().unwrap()[data.index].name)
         }));
 
     Flex::column()
         .with_child(title)
         .with_child(Either::new(
             |data: &MyAgency, _env: &Env| data.selected,
-            List::new(route_ui).lens(MyAgency::routes),
+            List::new(move || route_ui(Rc::clone(&gtfs))).lens(MyAgency::routes),
             Flex::row(),
         ))
         .cross_axis_alignment(CrossAxisAlignment::Start)
         .padding((10., 0., 0., 0.))
 }
 
-fn main_widget() -> impl Widget<AppData> {
-    Scroll::new(List::new(agency_ui).lens(AppData::agencies))
+fn main_widget(gtfs: Rc<RawGtfs>) -> impl Widget<AppData> {
+    Scroll::new(List::new(move || agency_ui(Rc::clone(&gtfs))).lens(AppData::agencies))
 }
 
-fn make_initial_data(gtfs: RawGtfs) -> AppData {
-    let mut agencies = gtfs.agencies.unwrap();
-    let mut routes = gtfs.routes.unwrap();
-    let mut trips = gtfs.trips.unwrap();
-    let mut stop_times = gtfs.stop_times.unwrap();
-    let mut stops = gtfs.stops.unwrap();
+fn make_initial_data(gtfs: Rc<RawGtfs>) -> AppData {
+    let mut agencies = gtfs.agencies.as_ref().unwrap().clone();
+    let mut routes = gtfs.routes.as_ref().unwrap().clone();
+    let mut trips = gtfs.trips.as_ref().unwrap().clone();
+    let mut stop_times = gtfs.stop_times.as_ref().unwrap().clone();
+    let mut stops = gtfs.stops.as_ref().unwrap().clone();
 
     // need to be able to grab a slice of stop times by trip id to avoid doing the below loads of times:
     // stop_times
@@ -183,13 +191,16 @@ fn make_initial_data(gtfs: RawGtfs) -> AppData {
     let app_data = AppData {
         agencies: agencies
             .iter()
-            .map(|agency| {
+            .enumerate()
+            .map(|(agency_index, agency)| {
                 let mut routes = routes
                     .iter()
-                    .filter(|route| route.agency_id == agency.id)
-                    .map(|route| MyRoute {
+                    .enumerate()
+                    .filter(|(_, route)| route.agency_id == agency.id)
+                    .map(|(index, route)| MyRoute {
                         selected: false,
-                        route: route.clone(),
+                        index,
+                        // route: route.clone(),
                         trips: trips
                             .iter()
                             .enumerate()
@@ -199,40 +210,44 @@ fn make_initial_data(gtfs: RawGtfs) -> AppData {
                                     stop_time_range_from_trip_id.get(&trip.id).unwrap().clone();
                                 let mut stops = stop_times[start_index..end_index]
                                     .iter()
+                                    .enumerate()
                                     // .filter(|stop_time| stop_time.trip_id == trip.id)
-                                    .map(|stop_time| MyStopTime {
+                                    .map(|(st_index, stop_time)| MyStopTime {
                                         selected: false,
-                                        stop_sequence: stop_time.stop_sequence,
+                                        // stop_sequence: stop_time.stop_sequence,
                                         // stop_time: Rc::new(stop_time.clone()),
                                         // stop_time: stop_time.clone(),
-                                        name: stop_map
-                                            .get(&stop_time.stop_id)
-                                            .unwrap()
-                                            .name
-                                            .clone(),
+                                        // name: stop_map
+                                        //     .get(&stop_time.stop_id)
+                                        //     .unwrap()
+                                        //     .name
+                                        //     .clone(),
+                                        index: st_index + start_index,
                                     })
                                     .collect::<Vector<_>>();
-                                stops.sort_by(|stop1, stop2| {
-                                    stop1.stop_sequence.cmp(&stop2.stop_sequence)
-                                });
+                                // stops.sort_by(|stop1, stop2| {
+                                //     stop1.stop_sequence.cmp(&stop2.stop_sequence)
+                                // });
 
                                 // adding the RawTrip to MyTrip is the tipping point which kills performance. Maybe AppData should just be storing a u32 index of the items position in the original RawGtfs data
                                 MyTrip {
                                     selected: false,
                                     // trip: Rc::new(trip.clone()),
-                                    name: trip.id.clone(),
+                                    index: i,
+                                    // name: trip.id.clone(),
                                     stops,
                                 }
                             })
                             .collect::<Vector<_>>(),
                     })
                     .collect::<Vector<_>>();
-                routes.sort_by(|route1, route2| {
-                    route1.route.short_name.cmp(&route2.route.short_name)
-                });
+                // routes.sort_by(|route1, route2| {
+                //     route1.route.short_name.cmp(&route2.route.short_name)
+                // });
                 MyAgency {
                     selected: false,
-                    agency: agency.clone(),
+                    index: agency_index,
+                    // agency: agency.clone(),
                     routes,
                 }
             })
@@ -245,14 +260,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = CliArgs::parse();
 
     println!("reading raw gtfs");
-    let gtfs = RawGtfs::new(&args.path.expect("must provide a path or url to a GTFS zip"))?;
+    let gtfs = Rc::new(RawGtfs::new(
+        &args.path.expect("must provide a path or url to a GTFS zip"),
+    )?);
     gtfs.print_stats();
 
     println!("making initial data");
-    let initial_data = make_initial_data(gtfs);
+    let initial_data = make_initial_data(Rc::clone(&gtfs));
 
     println!("making main window");
-    let main_window = WindowDesc::new(main_widget())
+    let main_window = WindowDesc::new(main_widget(Rc::clone(&gtfs)))
         .title("Select")
         .window_size((1000., 600.));
 
